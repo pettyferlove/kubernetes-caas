6b69dad6290b0aa2bd0fd6b7fc87aaebfa2175c3
Alex Pettyfer <pettyferlove@live.cn>
2020-06-01 11:16:17 +0800

    :tada: Initial commit.

--- src/main/java/com/github/pettyfer/kubernetes/restful/NamespaceController.java
+++ src/main/java/com/github/pettyfer/kubernetes/restful/NamespaceController.java
@@ -1,10 +1,5 @@
 package com.github.pettyfer.kubernetes.restful;
 
-import io.kubernetes.client.openapi.apis.CoreV1Api;
-import io.kubernetes.client.openapi.models.V1DeleteOptions;
-import io.kubernetes.client.openapi.models.V1Namespace;
-import io.kubernetes.client.openapi.models.V1ObjectMeta;
-import io.kubernetes.client.openapi.models.V1Status;
 import lombok.SneakyThrows;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,35 +10,15 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/v1/namespace")
 public class NamespaceController {
 
-    private final CoreV1Api coreV1Api;
-
-    public NamespaceController(CoreV1Api coreV1Api) {
-        this.coreV1Api = coreV1Api;
-    }
-
     @PostMapping("{namespace}")
     @SneakyThrows
-    public V1Namespace create(@PathVariable String namespace) {
-        V1Namespace v1Namespace = new V1Namespace();
-        v1Namespace.setKind("Namespace");
-        v1Namespace.setApiVersion("v1");
-        v1Namespace.setMetadata(new V1ObjectMeta().name(namespace));
-        return coreV1Api.createNamespace(v1Namespace, null, null, null);
+    public String create(@PathVariable String namespace) {
+        return "success";
     }
 
     @DeleteMapping("{namespace}")
     @SneakyThrows
     public String delete(@PathVariable String namespace) {
-        V1Namespace v1Namespace = new V1Namespace();
-        v1Namespace.setKind("Namespace");
-        v1Namespace.setApiVersion("v1");
-        v1Namespace.setMetadata(new V1ObjectMeta().name(namespace));
-        Integer gracePeriodSeconds = 0;
-        Boolean orphanDependents = true;
-        V1DeleteOptions options = new V1DeleteOptions();
-        options.setApiVersion("v1");
-        V1Status status = coreV1Api.deleteNamespace(namespace, null, null, gracePeriodSeconds, orphanDependents, null, options);
-        System.out.println(status);
         return "success";
     }
 
--- src/main/java/com/github/pettyfer/kubernetes/restful/PodController.java
+++ src/main/java/com/github/pettyfer/kubernetes/restful/PodController.java
@@ -1,7 +1,5 @@
 package com.github.pettyfer.kubernetes.restful;
 
-import io.kubernetes.client.openapi.apis.CoreV1Api;
-import io.kubernetes.client.openapi.models.V1PodList;
 import lombok.SneakyThrows;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,18 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/pod")
 public class PodController {
-
-    private final CoreV1Api coreV1Api;
-
-    public PodController(CoreV1Api coreV1Api) {
-        this.coreV1Api = coreV1Api;
-    }
-
     @GetMapping("list")
     @SneakyThrows
-    public V1PodList list() {
-        return coreV1Api.listNamespacedPod("default", null, null, null, null, null, null,
-                null, null, null);
+    public String list() {
+        return "success";
     }
 
 }
