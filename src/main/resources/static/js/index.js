7d24988eb6f8f1839f875cdaa20fd95e0bdd68f3
pettyferlove@live.cn <liuyang19950123>
2020-06-10 10:43:42 +0800

    移除静态页面

--- src/main/resources/static/js/index.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-    layui.use('element', function(){
-        let element = layui.element; //导航的hover效果、二级菜单等功能，需要依赖element模块
-        //监听导航点击
-        element.on('nav(menu)', function(elem){
-            $("#main-content").attr("src", elem.attr("url")||"dashboard")
-        });
-    });
-})
