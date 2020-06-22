13c3e6f2a9374507312471204aefc230fa9414b1
pettyferlove@live.cn <liuyang19950123>
2020-06-22 11:27:53 +0800

    :white_check_mark: 测试Maven

---
+++ src/test/java/com/github/pettyfer/kubernetes/MavenTests.java
@@ -0,0 +1,27 @@
+package com.github.pettyfer.kubernetes;
+
+import lombok.SneakyThrows;
+import org.apache.maven.shared.invoker.*;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.util.Collections;
+import java.util.Properties;
+
+public class MavenTests {
+
+    @Test
+    @SneakyThrows
+    public void mavenPackageTest(){
+        Invoker invoker = new DefaultInvoker();
+        invoker.setMavenHome(new File("D:/Maven"));
+        InvocationRequest request = new DefaultInvocationRequest();
+        request.setPomFile(new File("D:/CAAS/workspace/demo/pom.xml"));
+        request.setGoals( Collections.singletonList( "package" ) );
+        Properties properties = new Properties();
+        properties.setProperty("skipTests", "true");
+        request.setProperties(properties);
+        invoker.execute(request);
+    }
+
+}
