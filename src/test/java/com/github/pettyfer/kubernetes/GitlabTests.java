b110abd8136dbca6dc88429da5ae2065d8e81f45
pettyferlove@live.cn <liuyang19950123>
2020-06-24 17:38:37 +0800

    :white_check_mark: GitlabTests

--- src/test/java/com/github/pettyfer/kubernetes/GitlabTests.java
+++ src/test/java/com/github/pettyfer/kubernetes/GitlabTests.java
@@ -3,16 +3,27 @@ package com.github.pettyfer.kubernetes;
 import lombok.SneakyThrows;
 import org.gitlab.api.GitlabAPI;
 import org.gitlab.api.models.GitlabProject;
-import org.gitlab.api.models.GitlabVersion;
+import org.gitlab.api.models.GitlabProjectHook;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class GitlabTests {
+
+    private final String HOOKS_ENDPOINT = "http://192.168.51.67:8885/api/v1/hooks/gitlab/";
 
     @Test
     @SneakyThrows
     public void gitlabProjectsTest() {
         GitlabAPI connect = GitlabAPI.connect("http://gitlab.ggjs.sinobest.cn/", "sxynF2-S5_owPwGJh5su");
-        GitlabProject liuyang03813 = connect.getProject("liuyang03813", "auto-build-examples");
+        GitlabProject project = connect.getProject("liuyang03813", "auto-build-examples");
+        String projectHookUrl = HOOKS_ENDPOINT + project.getName();
+        List<GitlabProjectHook> projectHooks = connect.getProjectHooks(project);
+        List<GitlabProjectHook> collect = projectHooks.stream().filter(i -> projectHookUrl.equals(i.getUrl())).collect(Collectors.toList());
+        if(!(collect.size() >0)){
+            connect.addProjectHook(project, projectHookUrl);
+        }
     }
 
 }
